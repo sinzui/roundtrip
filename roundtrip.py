1e28e57fa4635494ffbe39a54a384c5d3061f531
Curtis Hovey <curtis.hovey@canonical.com>
2017-10-01 12:37:59 -0400

    Refactored script for testing.

--- roundtrip.py
+++ roundtrip.py
@@ -10,19 +10,31 @@ import sys
 import googlemaps
 
 
-API_KEY = os.environ.get('GOOGLE_API_KEY')
-if not API_KEY:
-    print('source/export GOOGLE_API_KEY before running script.')
-    sys.exit(1)
+def get_api_key():
+    return os.environ.get('GOOGLE_API_KEY')
 
-maps = googlemaps.Client(key=API_KEY)
-now = datetime.now()
-origin = "9402 Meadow Crossing Way, Fairfax Station, VA 22039"
-destination = "12193 Fair Lakes Promenade Dr, Fairfax, VA 22033"
-destinations = [destination]
-for destination in destinations:
-    result_routes = maps.directions(
-        origin, destination, mode="driving", departure_time=now)
-    distance = result_routes[0]['legs'][0]['distance']['value']
-    trip = round(distance * 2 / 5280, 2)
-    print('{}:\t{}'.format(destination, trip))
+
+def print_round_trips(client, origin, destinations):
+    for destination in destinations:
+        now = datetime.now()
+        result_routes = client.directions(
+            origin, destination, mode="driving", departure_time=now)
+        distance = result_routes[0]['legs'][0]['distance']['value']
+        trip = round(distance * 2 / 5280, 2)
+        print('{}:\t{}'.format(destination, trip))
+
+
+def main():
+    API_KEY = get_api_key()
+    if not API_KEY:
+        print('source/export GOOGLE_API_KEY before running script.')
+        sys.exit(1)
+
+    client = googlemaps.Client(key=API_KEY)
+    origin = "9402 Meadow Crossing Way, Fairfax Station, VA 22039"
+    destinations = ["12193 Fair Lakes Promenade Dr, Fairfax, VA 22033"]
+    print_round_trips(client, origin, destinations)
+
+
+if __name__ == '__main__':
+    main()
